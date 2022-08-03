f2cf9a6245f436a3399d1735e06d8a9af27a7ffc
andi-prasetyo <andiprasetyo700@gmail.com>
2022-08-03 23:46:49 +0700

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className=' w-[80%] mx-auto text-justify flex justify-between text-gray-400 my-24 text-sm'>
+        <div className='flex flex-col cursor-pointer'>
+            <p>Andi Prasetyo</p>
+            <p>152235865100-720-dts-final-project</p>        
+        </div>        
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/container/Profile.js
@@ -0,0 +1,16 @@
+import { Box } from '@mui/material';
+import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+import { auth } from '../config/firebase';
+
+const Profile = () => {
+  const [user] = useAuthState(auth);
+  return (
+    <Box sx={{ margin: 10 }}>
+      Welcome {user.email}
+    </Box>
+  )
+}
+
+export default Profile
